7ebed8aef86d086a889d2de3ff0fc78353ff4566
Theo Sanderson <theo@sndrsn.co.uk>
2020-12-06 01:49:53 +0000

    Update index.js

--- index.js
+++ index.js
@@ -287,7 +287,7 @@ Vue.component('needed_amount', {
 
   },
   template: `<div style="display:inline-block" class="computed">
-  <div class="button_holder_flask"><i class="fas change-input-type-button" :class="input_type_button_image" v-on:click="toggleType()"></i></div>
+  <div class="button_holder_flask"><i class="fas change-input-type-button toggler" :class="input_type_button_image" v-on:click="toggleType()" title="Toggle between measuring out mass and volume"></i></div>
   <div style="display:inline-block" class="weight_input" v-if="input_method=='weight'">
   <div class="needed_number" :title="needed_amount_mass[1]">{{needed_amount_mass[0]}}</div><unit type="mass"  v-model="mass_unit"/>
   </div>
@@ -659,5 +659,4 @@ var app = new Vue({
 });
 
 
-$(document).tooltip({show: null});
-
+$('.ttip').tooltip();
